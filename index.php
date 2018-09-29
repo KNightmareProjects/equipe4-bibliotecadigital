562f7e24d3522df5537549d7d2c1ceecc7d20dbf
KNightmareProjects <andreicalegari1@gmail.com>
2018-09-29 03:28:40 -0300

    Versão 1.3

--- index.php
+++ index.php
@@ -43,15 +43,16 @@
 			<!-- Footer -->
 				<footer id="footer">
 					<div class="inner">
-						<h2>Etiam veroeros lorem</h2>
-						<p>Pellentesque eleifend malesuada efficitur. Curabitur volutpat dui mi, ac imperdiet dolor tincidunt nec. Ut erat lectus, dictum sit amet lectus a, aliquet rutrum mauris. Etiam nec lectus hendrerit, consectetur risus viverra, iaculis orci. Phasellus eu nibh ut mi luctus auctor. Donec sit amet dolor in diam feugiat venenatis. </p>
-
+						<h2>Agradecemos a preferência</h2>
+						<p>[Hackaton FW 2018]</p>
+						<!--
 						<ul class="icons">
 							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
 							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
 							<li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
 						</ul>
+						-->
 						<p class="copyright">&copy; Copyright 2018 | KNightmare Projects</a>.</p>
 					</div>
 				</footer>
